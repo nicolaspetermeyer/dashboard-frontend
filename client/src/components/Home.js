fbe111f2b6ceeec374815cce4c98b55fae0902da
nicolaspetermeyer <nicolas.meyer1@outlook.com>
2023-07-23 21:38:55 +0200

    1

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,19 +5,11 @@ import ImageData from "./ImageData";
 
 const Home = () => {
   const slides = [
-<<<<<<< HEAD
-    { url: "http://localhost:3001/image1.png", title: "Image 1" },
-    { url: "http://localhost:3001/image2.png", title: "Image 2" },
-    { url: "http://localhost:3001/image3.png", title: "Image 3" },
-    { url: "http://localhost:3001/image4.png", title: "Image 4" },
-    { url: "http://localhost:3001/image5.png", title: "Image 5" },
-=======
     { url: "http://localhost:3000/images/image1.png", title: "Image 1" },
-    { url: "http://localhost:3000/image2.png", title: "Image 2" },
-    { url: "http://localhost:3000/image3.png", title: "Image 3" },
-    { url: "http://localhost:3000/image4.png", title: "Image 4" },
-    { url: "http://localhost:3000/image5.png", title: "Image 5" },
->>>>>>> a5899e5466758f2f5f03f2c5a797933c4ee658e5
+    { url: "http://localhost:3000/images/image2.png", title: "Image 2" },
+    { url: "http://localhost:3000/images/image3.png", title: "Image 3" },
+    { url: "http://localhost:3000/images/image4.png", title: "Image 4" },
+    { url: "http://localhost:3000/images/image5.png", title: "Image 5" },
   ];
 
   const containerStyles = {
